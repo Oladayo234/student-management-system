edae0e0144087603623ad8651e13a42ceb00919f
Oladayo234 <oladayoolubunmi20@gmail.com>
2025-12-08 06:00:05 +0100

    pizzaWahala

--- Pizza Wahala/pizza_store.js
+++ Pizza Wahala/pizza_store.js
@@ -1,7 +1,4 @@
-import java.util.Scanner;
-public class PizzaStore{
-    public static void main(String[]args){
-       System.out.print("""    
+  System.out.print("""    
                 Hello,
                 
                                 Welcome to Iya Scambirah Pizza joint
@@ -28,36 +25,34 @@ public class PizzaStore{
 
                 """);
 
-
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter number of guest; ");
-    int numberOfGuest = input.nextInt();
-    System.out.println("Enter pizza type; ");
-    String pizzaType = input.next().toLowerCase();
-    int numberOfSlices = 0;
-    int pricePerBox = 0;
+    const input = require('prompt-sync')();
+    let pizzaType = input('Enter pizza type: ');
+    let numberOfGuest = input('Enter number of guest: ');
+ 
+    let numberOfSlices = 0;
+    let pricePerBox = 0;
 
 
-    if (pizzaType.equals("sapa size")){
+    if (pizzaType === "sapa size")){
        numberOfSlices = 4;
        pricePerBox = 2000;}
 
-    else if (pizzaType.equals("small money")){
+    else if (pizzaType === "small money")){
             numberOfSlices = 6;
             pricePerBox = 2400;}
 
-    else if (pizzaType.equals("big boys")){
+    else if (pizzaType === "big boys")){
             numberOfSlices = 8;
             pricePerBox = 3000;}
 
-    else if (pizzaType.equals("odogwu")){
+    else if (pizzaType === ("odogwu")){
             numberOfSlices = 12;
             pricePerBox = 4200;}
 
-    int numberOfBoxes = (int) Math.ceil((double)numberOfGuest / numberOfSlices);
-    int remainder = (numberOfSlices * numberOfBoxes) - numberOfGuest;
-    int price = pricePerBox * numberOfBoxes;
-    int total = numberOfBoxes * numberOfSlices;
+    let numberOfBoxes = Math.ceil(numberOfGuest / numberOfSlices);
+    let remainder = (numberOfSlices * numberOfBoxes) - numberOfGuest;
+    let price = pricePerBox * numberOfBoxes;
+    let total = numberOfBoxes * numberOfSlices;
 
 
     System.out.printf("price = %d.%n%d per box for %d boxes.", price, pricePerBox, numberOfBoxes);
@@ -65,6 +60,6 @@ public class PizzaStore{
     System.out.printf("Total slice: %d.%nRemaining slices: %d%n", total, remainder);
 
     System.out.printf("%s pizza contains %d per box.%n%d boxes should be sufficient for %d people as it would contain %d slices in all%n", pizzaType, numberOfSlices, numberOfBoxes, numberOfGuest, total);
-    }
 
-}
+
+
