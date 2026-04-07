b8f1e2382820de3cd03597359edaaba976301c03
Oladayo234 <oladayoolubunmi20@gmail.com>
2026-04-07 08:12:11 +0100

    student management system

--- app/main.py
+++ app/main.py
@@ -1,19 +1,10 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from app.database import client
 from app.routes.user_routes import router as user_router
 from app.routes.course_routes import router as course_router
 from app.routes.enrollment_routes import router as enrollment_router
 from app.exceptions.handlers import register_exception_handlers
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Connected to MongoDB")
-    yield
-    client.close()
-    print("Disconnected from MongoDB")
-
-app = FastAPI(title="Student Course Management System", lifespan=lifespan)
+app = FastAPI(title="Student Course Management System")
 
 register_exception_handlers(app)
 
